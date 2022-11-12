6744411e74335536714f11098e86ffe30ed0535b
SubinNoh <snoh6839@mysvc.skagit.edu>
2022-11-12 14:08:28 +0900

    hadergnb

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,9 @@
 'use strict'
 
 $(function(){
-	
+
+
+ //슬릭 슬라이더 사용
 	$('.weather-tic').slick({
 		vertical : true,
 		dots : false,
@@ -11,4 +13,16 @@ $(function(){
 	})
 
 
+
+	//언어 선택 버튼
+		$(".lang-btn .dropdown-menu").hide();
+		$(".lang-btn a").click(function(){
+			if($(".lang-btn .dropdown-menu").css("display") != "none"){
+				$(".lang-btn .dropdown-menu").fadeOut();
+			}else{
+				$(".lang-btn .dropdown-menu").fadeIn();
+			}
+		});
+
+		
 });
